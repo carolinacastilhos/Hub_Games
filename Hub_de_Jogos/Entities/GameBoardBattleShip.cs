2071c142138395c83de477373e9b2d31b195a8dc
Carolina Castilhos <carolinacastilhos.vet@gmail.com>
2023-01-30 22:37:37 -0300

    updated BattleShip Gameboard

--- Hub_de_Jogos/Entities/GameBoardBattleShip.cs
+++ Hub_de_Jogos/Entities/GameBoardBattleShip.cs
@@ -13,30 +13,24 @@ namespace Hub_de_Jogos.Entities
         public static string[,] gameBoardPlayer2 = new string[8, 8];
 
         public static string[,] InitializeGameBoardPlayer1()
-        {
-            int counter = 1;
-                        
+        {                                    
             for (int row = 0; row < 8; row++)
             {
                 for (int col = 0; col < 8; col++)
                 {
-                    gameBoardPlayer1[row, col] += $"{counter}";
-                    counter++;                                        
+                    gameBoardPlayer1[row, col] += "~";                                                            
                 }
             }
-            return gameBoardPlayer1;
+            return gameBoardPlayer1;                                
         }
 
         public static string[,] InitializeGameBoardPlayer2()
-        {
-            int counter = 1;
-            
+        {                        
             for (int row = 0; row < 8; row++)
             {
                 for (int col = 0; col < 8; col++)
                 {
-                    gameBoardPlayer2[row, col] = $"{counter}";
-                    counter++;
+                    gameBoardPlayer2[row, col] += "~";
                 }
             }
             return gameBoardPlayer2;
@@ -44,50 +38,116 @@ namespace Hub_de_Jogos.Entities
 
         public static void DisplayGameBoardPlayer1()
         {
-            Console.WriteLine(" ____  ____  ____  ____  ____  ____  ____  ____ ");
+            Console.WriteLine("    0   1   2   3   4   5   6   7   ");
+            Console.WriteLine("   ___ ___ ___ ___ ___ ___ ___ ___ ");
             for (int row = 0; row < 8; row++)
             {
-                for (int col = 0; col < 8; col++)
+                if (row == 0)
                 {
-                    Console.Write("| ");
-                    Console.Write($"{gameBoardPlayer1[row, col]}");                                    
-                    Console.Write(" |");
+                    Console.Write($"0 | ");
                 }
+                if (row == 1)
+                {
+                    Console.Write($"1 | ");
+                }
+                if (row == 2)
+                {
+                    Console.Write($"2 | ");
+                }
+                if (row == 3)
+                {
+                    Console.Write($"3 | ");
+                }
+                if (row == 4)
+                {
+                    Console.Write($"4 | ");
+                }
+                if (row == 5)
+                {
+                    Console.Write($"5 | ");
+                }
+                if (row == 6)
+                {
+                    Console.Write($"6 | ");
+                }
+                if (row == 7)
+                {
+                    Console.Write($"7 | ");
+                }              
+                
+                for (int col = 0; col < 8; col++)
+                {                  
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.Write($"{gameBoardPlayer1[row, col]}");
+                    Console.ResetColor();
+                    Console.Write(" | ");
+                }                
                 Console.WriteLine();
-                Console.WriteLine("|----||----||----||----||----||----||----||----|");
+                Console.WriteLine("  |---|---|---|---|---|---|---|---|");
             }   
         }
 
         public static void DisplayGameBoardPlayer2()
         {
-            Console.WriteLine(" ____  ____  ____  ____  ____  ____  ____  ____ ");
+            Console.WriteLine("    0   1   2   3   4   5   6   7   ");
+            Console.WriteLine("   ___ ___ ___ ___ ___ ___ ___ ___ ");
             for (int row = 0; row < 8; row++)
             {
+                if (row == 0)
+                {
+                    Console.Write($"0 | ");
+                }
+                if (row == 1)
+                {
+                    Console.Write($"1 | ");
+                }
+                if (row == 2)
+                {
+                    Console.Write($"2 | ");
+                }
+                if (row == 3)
+                {
+                    Console.Write($"3 | ");
+                }
+                if (row == 4)
+                {
+                    Console.Write($"4 | ");
+                }
+                if (row == 5)
+                {
+                    Console.Write($"5 | ");
+                }
+                if (row == 6)
+                {
+                    Console.Write($"6 | ");
+                }
+                if (row == 7)
+                {
+                    Console.Write($"7 | ");
+                }
+
                 for (int col = 0; col < 8; col++)
                 {
-                    Console.Write("| ");
-                    /*if (gameBoardPlayer2[row, col] == "X")
+                    if (gameBoardPlayer2[row, col] == "N")
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
                         Console.Write($"{gameBoardPlayer2[row, col]}");
                         Console.ResetColor();
                     }
-                    else if (gameBoardPlayer1[row, col] == "O")
+                    else if (gameBoardPlayer1[row, col] == "X")
                     {
-                        Console.ForegroundColor = ConsoleColor.DarkCyan;
-                        Console.BackgroundColor = ConsoleColor.DarkCyan;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.Write($"{gameBoardPlayer2[row, col]}");
                         Console.ResetColor();
                     }
                     else
-                    {*/
-                        Console.Write($"{gameBoardPlayer2[row, col]}");
-                    //}
-                    Console.Write(" |");
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.Write($"{gameBoardPlayer2[row, col]}");
+                    Console.ResetColor();
+                    Console.Write(" | ");
                 }
                 Console.WriteLine();
-                Console.WriteLine("|----||----||----||----||----||----||----||----|");
+                Console.WriteLine("  |---|---|---|---|---|---|---|---|");
             }
         }
     }
--- Hub_de_Jogos/Service/BattleShip.cs
+++ Hub_de_Jogos/Service/BattleShip.cs
@@ -1,5 +1,7 @@
 ﻿using Hub_de_Jogos.Entities;
+using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
 
 namespace Hub_de_Jogos.Service
 {
@@ -14,7 +16,7 @@ namespace Hub_de_Jogos.Service
             Console.WriteLine("\tThe enemy is attacking us by the sea!");
             Console.WriteLine("\tWe have to choose some coordenates to drop bombs on their positions.");
 
-            Console.WriteLine("\nhey, we need to be smart... We only have 10 bombs and they have 5 ships there!");
+            Console.WriteLine("\n\they, we need to be smart... We only have 10 bombs and they have 5 ships there!");
             Console.WriteLine("\tIf we're lucky, we'll hit one of their battleships.");                     
 
             Console.WriteLine("\n\tSo let's go!\n");                        
@@ -32,6 +34,7 @@ namespace Hub_de_Jogos.Service
             Console.WriteLine("                BATTLESHIP!             ");
             //Console.WriteLine("________________________________________\n");
             Console.ResetColor();
+            Console.WriteLine();
         }
 
         public static void StartBattleShip()
@@ -45,29 +48,90 @@ namespace Hub_de_Jogos.Service
             player2.Nickname = "Bruno";
             Player currentPlayer = player1;
 
+            GameBoardBattleShip.InitializeGameBoardPlayer1();
+            GameBoardBattleShip.DisplayGameBoardPlayer1();
 
-            Console.Write("\n{0}, enter the position you want to drop a bomb: ", currentPlayer.Nickname);
-            int.TryParse(Console.ReadLine(), out int playerMove);
+            Console.WriteLine("\n{0}, what are the coordenates you want to drop a bomb?", currentPlayer.Nickname);
+            Console.Write("Please, enter the row: ");
+            int.TryParse(Console.ReadLine(), out int destinationRow);
+            Console.Write("Enter the column: ");
+            int.TryParse(Console.ReadLine(), out int destinationCol);
             Console.WriteLine();
-
-
-
-
-
-            /*int shipPlayer1Position;
-            int shipPlayer2Position;
-
-            Random random = new Random();
-            for (int i = 0; i < 4; i++)
+                        
+            /*
+            Console.WriteLine("    0   1   2   3   4   5   6   7   ");
+            Console.WriteLine("   ___ ___ ___ ___ ___ ___ ___ ___ ");
+            for (int row = 0; row < 8; row++)
             {
-                shipPlayer1Position = random.Next(1, 64);
+                if (row == 0)
+                {
+                    Console.Write($"0 | ");
+                }
+                if (row == 1)
+                {
+                    Console.Write($"1 | ");
+                }
+                if (row == 2)
+                {
+                    Console.Write($"2 | ");
+                }
+                if (row == 3)
+                {
+                    Console.Write($"3 | ");
+                }
+                if (row == 4)
+                {
+                    Console.Write($"4 | ");
+                }
+                if (row == 5)
+                {
+                    Console.Write($"5 | ");
+                }
+                if (row == 6)
+                {
+                    Console.Write($"6 | ");
+                }
+                if (row == 7)
+                {
+                    Console.Write($"7 | ");
+                }
 
-            }*/
+
+                for (int col = 0; col < 8; col++)
+                {
+                    if (GameBoardBattleShip.gameBoardPlayer1[destinationRow, destinationCol] == GameBoardBattleShip.gameBoardPlayer1[4, 8] || GameBoardBattleShip.gameBoardPlayer1[destinationRow, destinationCol] == GameBoardBattleShip.gameBoardPlayer1[5, 2] ||
+                        GameBoardBattleShip.gameBoardPlayer1[destinationRow, destinationCol] == GameBoardBattleShip.gameBoardPlayer1[2, 7] || GameBoardBattleShip.gameBoardPlayer1[destinationRow, destinationCol] == GameBoardBattleShip.gameBoardPlayer1[8, 0] ||
+                        GameBoardBattleShip.gameBoardPlayer1[destinationRow, destinationCol] == GameBoardBattleShip.gameBoardPlayer1[0, 1])
+                    {
+                        GameBoardBattleShip.gameBoardPlayer1[row, col] = "N";
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write($"{GameBoardBattleShip.gameBoardPlayer1[row, col]}"); 
+                        Console.ResetColor();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Wow! You hit a ship, {0}!", currentPlayer.Nickname);
+                        Console.ResetColor();                                               
+                    }
+                    else
+                    {
+                        GameBoardBattleShip.gameBoardPlayer1[row, col] = "X";
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"{GameBoardBattleShip.gameBoardPlayer1[row, col]}");
+                        Console.ResetColor();
+
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("Oops! You miss the ship, {0}...", currentPlayer.Nickname);
+                        Console.ResetColor();                        
+                    }                    
+                }
+                Console.WriteLine();
+                Console.WriteLine("  |---|---|---|---|---|---|---|---|");
+            }    */      
 
         }
 
-        
-        
+
+
 
     }
 }
